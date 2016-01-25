8947ac3a8741846cfcc9543830cb7942d4f4913f
Chris Fung <aergonaut@gmail.com>
2016-01-25 08:40:17 -0800

    expand test cases around permissions description checker

--- spec/mediators/check_new_permissions_include_descriptions_spec.rb
+++ spec/mediators/check_new_permissions_include_descriptions_spec.rb
@@ -13,30 +13,7 @@ RSpec.describe CheckNewPermissionsIncludeDescriptions do
   let(:pull_request_files_body) { File.open(Rails.root.join("spec", "fixtures", "permission_changes.json")) }
 
   let(:permissions_yml_contents) do
-    hash = {
-      "squiggle_transfers__bulk_loader" => {
-        "id" => 4242,
-        "controller" => "squiggle_transfers",
-        "action" => "bulk_loader",
-        "description" => "",
-        "flavor" => 4
-      },
-      "squiggle_transfers__list_csv" => {
-        "id" => 4243,
-        "controller" => "squiggle_transfers",
-        "action" => "list_csv",
-        "description" => "",
-        "flavor" => 4
-      },
-      "squiggle_transfers__load_progress" => {
-        "id" => 4244,
-        "controller" => "squiggle_transfers",
-        "action" => "load_progress",
-        "description" => "",
-        "flavor" => 4
-      }
-    }
-    encoded_contents = Base64.encode64(YAML.dump(hash))
+    encoded_contents = Base64.encode64(YAML.dump(permissions_yml_body))
 
     from_fixture = JSON.load(File.open(Rails.root.join("spec", "fixtures", "permissions_contents.json")))
     from_fixture["content"] = encoded_contents
@@ -62,9 +39,99 @@ RSpec.describe CheckNewPermissionsIncludeDescriptions do
   let(:job) { CheckNewPermissionsIncludeDescriptions.new }
 
   describe "#perform" do
-    context "when some permissions have blank descriptions" do
-      it "emits a warning" do
+    context "when all permissions have blank descriptions" do
+      let(:permissions_yml_body) do
+        {
+          "squiggle_transfers__bulk_loader" => {
+            "id" => 4242,
+            "controller" => "squiggle_transfers",
+            "action" => "bulk_loader",
+            "description" => "",
+            "flavor" => 4
+          },
+          "squiggle_transfers__list_csv" => {
+            "id" => 4243,
+            "controller" => "squiggle_transfers",
+            "action" => "list_csv",
+            "description" => "",
+            "flavor" => 4
+          },
+          "squiggle_transfers__load_progress" => {
+            "id" => 4244,
+            "controller" => "squiggle_transfers",
+            "action" => "load_progress",
+            "description" => "",
+            "flavor" => 4
+          }
+        }
+      end
+
+      it "emits 3 warnings" do
         expect(job.perform(payload).count).to eq(3)
+      end
+    end
+
+    context "when some permissions have blank descriptions" do
+      let(:permissions_yml_body) do
+        {
+          "squiggle_transfers__bulk_loader" => {
+            "id" => 4242,
+            "controller" => "squiggle_transfers",
+            "action" => "bulk_loader",
+            "description" => "lorem ipsum",
+            "flavor" => 4
+          },
+          "squiggle_transfers__list_csv" => {
+            "id" => 4243,
+            "controller" => "squiggle_transfers",
+            "action" => "list_csv",
+            "description" => "",
+            "flavor" => 4
+          },
+          "squiggle_transfers__load_progress" => {
+            "id" => 4244,
+            "controller" => "squiggle_transfers",
+            "action" => "load_progress",
+            "description" => "",
+            "flavor" => 4
+          }
+        }
+      end
+
+      it "emits 2 warnings" do
+        expect(job.perform(payload).count).to eq(2)
+      end
+    end
+
+    context "when no permissions have blank descriptions" do
+      let(:permissions_yml_body) do
+        {
+          "squiggle_transfers__bulk_loader" => {
+            "id" => 4242,
+            "controller" => "squiggle_transfers",
+            "action" => "bulk_loader",
+            "description" => "lore",
+            "flavor" => 4
+          },
+          "squiggle_transfers__list_csv" => {
+            "id" => 4243,
+            "controller" => "squiggle_transfers",
+            "action" => "list_csv",
+            "description" => "ipsum",
+            "flavor" => 4
+          },
+          "squiggle_transfers__load_progress" => {
+            "id" => 4244,
+            "controller" => "squiggle_transfers",
+            "action" => "load_progress",
+            "description" => "dolor",
+            "flavor" => 4
+          }
+        }
+      end
+
+      it "emits a warning" do
+        expect(job.perform(payload)).to be_empty
       end
     end
   end
